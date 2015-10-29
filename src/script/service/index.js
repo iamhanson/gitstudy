d15c285f6b068d3e506c1f6dba4652179fde5381
hanhui@haohandata.com.cn <hanhui@haohandata.com.cn>
2015-10-29 11:40:10 +0800

    no message

--- src/script/service/index.js
+++ src/script/service/index.js
@@ -1,7 +1,21 @@
-
+/**
+ * 这是一个测试模块
+ * @module mainIndex
+ * 
+ * @example mainIndex.functionName //获取该模块的方法或者属性
+ */
 var mainIndex=(function(_self){
 
-	
+	/**
+	* 初始化tab页
+	*/
+	_self.initTab=function(){
+		$('#tab-container').easytabs();
+	};
 
 	return _self;
 })(window.mainIndex||{});
+
+$(function(){
+	mainIndex.initTab();
+});
